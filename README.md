4308f84c919e3bd1e3c0d998e95814151688e29f
tm.1998 <tm.1998@163.com>
2020-09-15 00:07:24 -0700

    addition

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # COSC499-git-practice
+This repo contains two branch features for addition and subtraction for the input number.
--- calculator.py
+++ calculator.py
@@ -2,3 +2,18 @@ print ('This is a test calculator for git practice')
 
 var1 = input('Enter the first number')
 var2 = input('Enter the second number')
+
+def addition(v1,v2):
+    v1 = var1
+    v2 = var2
+    v1 += v2
+    print('Addition:%d' % v1)
+
+
+#---------------------------------------
+import unittest
+
+unittest.assertEqual(addition(1,2), 3)
+
+if __name__ == '__main__':
+    unittest.main()
